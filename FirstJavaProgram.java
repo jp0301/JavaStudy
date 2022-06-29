fb49e4898ed8d6eda1e59abace9b8dd8e57d1d07
Onedoli <rjswm78@gmail.com>
2022-06-29 14:15:39 +0900

    Initial commit

---
+++ FirstJavaProgram.java
@@ -0,0 +1,14 @@
+
+class FirstJavaProgram {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Java");
+		System.out.println("First Java Program");
+	}
+}
+
+// ������
+/*
+ * Welcome to Java
+ * First Java Program
+ * ����Ϸ��� �ƹ� Ű�� �����ʽÿ� . . .
+ */
