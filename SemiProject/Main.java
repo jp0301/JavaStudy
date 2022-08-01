2636daaeed32101bf71f986195deb49be45274c8
Onedoli <rjswm78@gmail.com>
2022-08-01 22:33:46 +0900

    2022-08-01 update(2)

--- SemiProject/Main.java
+++ SemiProject/Main.java
@@ -2,7 +2,7 @@
 
 
 
-
+import java.util.Scanner;
 
 // 메인 클래스
 public class Main
@@ -11,12 +11,43 @@ public class Main
 	{
 		// 초기화면(메인 화면) 출력
 		Output op = new Output();
-		op.mainLogo();
-		op.startMenu();
 
-		
+		boolean loop = true;
 
-		
+		while(loop)
+		{
+			op.mainLogo();
+			op.startMenu();
+
+			Scanner sc = new Scanner(System.in);
+
+			System.out.println();
+			System.out.print("번호 입력(숫자) : ");
+			String number = sc.nextLine();
+
+			try
+			{
+				if(number.equals("1"))
+				{
+					
+				}
+				else if(number.equals("2"))
+				{
+
+				}
+			}
+			catch (Exception e)
+			{
+				System.out.println("잉?");
+				e.printStackTrace();
+			}
+
+		} // end while
+
+		System.out.println();
+
+
+
 
 	}
 }
--- SemiProject/Output.java
+++ SemiProject/Output.java
@@ -4,12 +4,13 @@ public class Output
 {
 	public static void mainLogo()
 	{
-		
+		System.out.println("\t\t\t==========================================");
+		System.out.println("\t\t\t\"¿À´Ã Á¡½É ¹¹ ¸ÔÁö?\" with ½Ö¿ë°­ºÏ±³À°¼¾ÅÍ");	
+		System.out.println("\t\t\t==========================================");
 	}
 
 	public static void startMenu()
 	{
-		System.out.println("\t\t\t\"¿À´Ã Á¡½É ¹¹ ¸ÔÁö?\" with ½Ö¿ë°­ºÏ±³À°¼¾ÅÍ");
 		System.out.print("\t\t\t¦£");
 		for(int i = 0; i < 39; i++) System.out.print("¦¡");
 		System.out.print("¦¤\n");
